67071ca1e61fa4a88f34be0bd9e27b2ebf3e78b6
jensen zhang <rasengun05@gmail.com>
2022-01-04 17:56:48 -0500

    old version of discord js to fix compat issues with node

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const Markov = require('./Markov.js');
 const markov = new Markov();
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// client.on('ready', () => {
-//     console.log(`logged in as ${client.user.tag}`);
-// })
+client.on('ready', () => {
+    console.log(`logged in as ${client.user.tag}`);
+})
 
+client.login('ODU2OTk3MzI2MDUxNjA2NTU4.YNJK9A.EBdORGnICSoaW_4e3MgPQtPtIkI').catch(err => {console.log(err);})
 
+// (async() => {
+//     await markov.loadChainFromFile();
 
-(async() => {
-    await markov.loadChainFromFile();
-    // await markov.generateChain('Hey, its me');
-    // await markov.generateChain('bruh moment this doesnt work');
-
-    console.log(markov.generateSentence());
-})();
+//     console.log(markov.generateSentence());
+// })();
