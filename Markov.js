9a2d60726cece38199c4f297f729852bad11f4c0
jensen zhang <rasengun05@gmail.com>
2022-01-04 16:50:37 -0500

    basic markov chain and sentence generation

--- Markov.js
+++ Markov.js
@@ -16,10 +16,12 @@ module.exports = class Markov {
     async generateChain() {
         const textArr = this.string.split(' ');
 
-        for (let i = 0; i < textArr.length - 2; i++) {
-            const key = `${textArr[i]} ${textArr[i + 1]}`;
-            const value = textArr[i + 2];
+        for (let i = 0; i < textArr.length - 1; i++) {
+            //using two words as key
+            const key = `${textArr[i].replace(/[\W_]/, "").toLowerCase()}`;
+            const value = textArr[i + 1].replace(/[\W_]/, "").toLowerCase();
 
+            // if the key already exists, add the value to already-existing key, else push the key and value to the chain
             if (this.chain[key]) {
                 this.chain[key].push(value);
             } else {
@@ -27,5 +29,46 @@ module.exports = class Markov {
             }
         }
         return this.chain;
+    }
+
+    getChain() {
+        return this.chain;
+    }
+
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    generateSentence() {
+        const keys = Object.keys(this.chain);
+        let startingWord = keys[Math.floor(Math.random() * keys.length)];
+        let sentence = ``;
+        console.log(this.chain);
+
+        for (let i = 0; i < this.getRandomInt(8, 18); i++) {
+            // console.log(i, startingWord);
+            sentence += startingWord + ' ';
+            let nextWord = this.chain[startingWord][Math.floor(Math.random() * this.chain[startingWord].length)];
+            // sentence += ` ${nextWord}`;
+            startingWord = nextWord;
+        }
+
+        return sentence;
+    }
+
+    generateSentence1() {
+        const keys = Object.keys(this.chain);
+        let sentence = '';
+
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            const values = this.chain[key];
+
+            const rand = Math.floor(Math.random() * values.length);
+            sentence += values[rand] + ' ';
+        }
+        return sentence;
     }
 }
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Markov = require('./Markov.js');
-let markov = new Markov();
-markov.setString('I am a cat');
-console.log(markov.getString());
+const markov = new Markov();
+
+
+(async() => {
+    markov.setString('No. The very first “alpha” version of the bot was sort of open source, however it wasn’t on any kind of GitLab or GitHub repository, it was just a single file on a Hastebin paste somewhere. That file has long since been deleted, and since then the bot has been closed source. Ideally I’d like to keep it that way, too. However if you’re a Python developer and you’d like to contribute to the bot in some way, do let me know on the support server.');
+
+    markov.generateChain();
+    console.log(markov.generateSentence());
+})();
